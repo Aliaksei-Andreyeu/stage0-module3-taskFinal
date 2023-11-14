a03012be080caabee6d2e9f1f5ff692a276461e9
aliaksei-andreyeu <aliaksei.andreyeu@gmail.com>
2023-11-14 19:25:59 +0300

    Submit Module 3 Final Task

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reverse = (number % 10);
+        number /= 10;
+        reverse = reverse * 10 +  (number % 10);
+        number /= 10;
+        reverse = reverse * 10 +  (number % 10);
+        System.out.println(reverse);
     }
 }
